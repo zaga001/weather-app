2c358162170f395ca16e3d8a4112440a7ca546aa
zaga <163298261+zaga001@users.noreply.github.com>
2025-04-14 14:47:13 +0700

    Refactor location search to use form submission instead of button click

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
-const button = document.getElementById('location-button');
-const locationInput = document.getElementById('location');
+const form = document.querySelector("form");
+const locationInput = document.getElementById("location");
+const content = document.getElementById("content");
 
-button.addEventListener('click', () => {
-    const locationValue = locationInput.value
-    getWeather(locationValue);
-})
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const locationValue = locationInput.value;
+  getWeather(locationValue);
+  form.reset();
+});
 
 async function getWeather(location) {
-    const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=JG55RBPYTP3JZSR3KGZHMBUFR`)
-    const data = await response.json();
-    console.log(data);
+  const response = await fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=JG55RBPYTP3JZSR3KGZHMBUFR`
+  );
+  const data = await response.json();
+  const processedData = processData(data);
+  console.log(processedData);
+  displayData(data);
+}
+
+function processData(data) {
+  return {
+    address: data.address,
+    currentConditions: data.currentConditions,
+    description: data.description,
+  };
+}
+
+function displayData(data) {
+  content.innerHTML = `
+  <p>${data.address}</p>
+  <p>${data.description}</p>
+  <p>Conditions: ${data.currentConditions.conditions}</p>
+  <p>Temperature: ${Math.round(
+    (data.currentConditions.temp - 32) / 1.8
+  )}&deg;C</p>
+  <p>Humidity: ${data.currentConditions.humidity}%`;
 }
